246b1ae8b07efdb59f988ae961774ba77d3acc9a
Max <>
2023-10-14 14:03:30 -0700

    create two new pose estimators and change apriltag weight

--- src/main/java/com/team766/ViSION/PoseEstimator.java
+++ src/main/java/com/team766/ViSION/PoseEstimator.java
@@ -7,11 +7,19 @@ import edu.wpi.first.math.geometry.Transform3d;
 
 
 public class PoseEstimator {
-	public PhotonPoseEstimator pEstimator = new PhotonPoseEstimator(
+	public PhotonPoseEstimator pEstimatorCam1 = new PhotonPoseEstimator(
 		SWingFieldTemplate.getFieldLayout(),
-		PoseStrategy.MULTI_TAG_PNP,
+		PoseStrategy.LOWEST_AMBIGUITY,
 		(PhotonCamera) Cameras.camera1,
 		new Transform3d());
 
-	
+	public PhotonPoseEstimator pEstimatorCam2 = new PhotonPoseEstimator(SWingFieldTemplate.getFieldLayout(), 
+		PoseStrategy.LOWEST_AMBIGUITY, 
+		(PhotonCamera) Cameras.camera2, 
+		new Transform3d());
+
+	public PhotonPoseEstimator pEstimatorCam3 = new PhotonPoseEstimator(SWingFieldTemplate.getFieldLayout(),
+		PoseStrategy.LOWEST_AMBIGUITY, 
+		(PhotonCamera) Cameras.camera3, 
+		new Transform3d());
 }
